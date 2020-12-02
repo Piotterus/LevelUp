facd1d25324deaef976b48514d31fe755629d77f
Piotr_Marcin Katański <piotr_marcin.katanski@student.put.poznan.pl>
2020-12-02 15:26:15 +0100

    background color

--- components/Footer.js
+++ components/Footer.js
@@ -170,7 +170,7 @@ const styles = StyleSheet.create({
     footerView: {
         flexDirection: 'row',
         backgroundColor: '#2592E6',
-        height: 75,
+        height: 80,
         marginTop: -5,
         zIndex: 0,
         justifyContent: 'space-around',
